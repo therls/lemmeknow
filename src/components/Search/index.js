c61b0a7036b7a07b5fd07f1cc0fb7510a7434cef
Robert Smith <67526754+therls@users.noreply.github.com>
2020-10-20 03:40:03 -0700

    nothing

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { SearchContainer, SearchContent, SearchTitle, SearchBar, SearchInput, Icon } from './SearchElements'
 
 const Search = () => {
+
     return (
         <SearchContainer>
             <SearchContent>
                 <SearchTitle>Lemmeknow</SearchTitle>
                 <SearchBar>
-                    <SearchInput placeholder="Search">
+                    <SearchInput type="text" name="search" placeholder="Search">
                     </SearchInput>
-                    <Icon></Icon> {/*Activate the "clear all" button.*/}
+                    <Icon></Icon>
                 </SearchBar>
             </SearchContent>
         </SearchContainer>
