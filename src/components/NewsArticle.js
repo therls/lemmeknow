3f3e126b252b3931d63fac02a90f80e24295573a
Robert Smith <67526754+therls@users.noreply.github.com>
2020-11-06 22:02:00 -0800

    Article titles are now href links.

--- src/components/NewsArticle.js
+++ src/components/NewsArticle.js
@@ -3,7 +3,7 @@ import React from 'react';
 function NewsArticle({ data }) {
   return (
     <div className="news">
-      <h1 className="news__title">{data.title}</h1>
+      <h1 className="news__title"><a href={data.url} target="_blank">{data.title}</a></h1>
       <p className="news__desc">{data.description}</p>
       <span className="news__author">{data.author}</span>
       <span className="news__published">{data.publishedAt}</span>
